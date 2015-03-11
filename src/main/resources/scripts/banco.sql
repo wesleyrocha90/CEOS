1248b8caf9f614e4c00500642821354865896862
Wesley Rocha <wesleyallanrocha@gmail.com>
2015-03-11 16:18:13 -0300

    Criado propriedades no pom.xml para armazenar as versões das bibliotecas, e acrescentado a biblioteca do Weld; Alterado script para inserção de dados iniciais no banco, excluído script para criação de tabelas, isso fica a cargo do JPA;

--- src/main/resources/scripts/banco.sql
+++ src/main/resources/scripts/banco.sql
@@ -1,52 +1,15 @@
--- CRIAÇÃO DE TODAS AS TABELAS
-
--- GRUPO_USUARIO
-CREATE SEQUENCE seq_grupo_usuario_id INCREMENT 1 START 1;
-CREATE TABLE grupo_usuario (
-  id int4 PRIMARY KEY DEFAULT nextval('seq_grupo_usuario_id'),
-  descricao VARCHAR(255) NOT NULL,
-  data_cadastro DATE NOT NULL
-);
-CREATE UNIQUE INDEX idx_grupo_usuario_id ON grupo_usuario(id);
-
--- USUARIO
-CREATE SEQUENCE seq_usuario_id INCREMENT 1 START 1;
-CREATE TABLE usuario (
-  id int4 PRIMARY KEY DEFAULT nextval('seq_usuario_id'),
-  login VARCHAR(255) NOT NULL,
-  senha VARCHAR(255) NOT NULL,
-  ativo BOOLEAN DEFAULT TRUE,
-  fk_grupo_usuario_id int4 REFERENCES grupo_usuario(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  data_cadastro DATE NOT NULL,
-  data_expiracao DATE
-);
-CREATE UNIQUE INDEX idx_usuario_id ON usuario(id);
-
 -- INSERÇÃO DE DADOS PADRÃO
 
-INSERT INTO grupo_usuario VALUES(DEFAULT, 'Administrador', CURRENT_DATE);
+INSERT INTO grupo_usuario(ID, DESCRICAO, DATA_CADASTRO) 
+  VALUES(DEFAULT, 'Administrador', CURRENT_DATE);
 
-INSERT INTO usuario VALUES(DEFAULT, 'wesley', '123', TRUE, 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 YEAR');
-INSERT INTO usuario VALUES(DEFAULT, 'marco', '123', TRUE, 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 YEAR');
-INSERT INTO usuario VALUES(DEFAULT, 'danilo', '123', TRUE, 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 YEAR');
+INSERT INTO usuario(ID, LOGIN, SENHA, ATIVO, FK_GRUPO_USUARIO_ID, DATA_CADASTRO, DATA_EXPIRACAO) 
+  VALUES(DEFAULT, 'wesley', '123', TRUE, 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 YEAR'),
+        (DEFAULT, 'marco', '123', TRUE, 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 YEAR');
 
-CREATE SEQUENCE seq_menu_painel_id INCREMENT 1 START 1;
-CREATE TABLE menu_painel (
-  id int4 PRIMARY KEY DEFAULT nextval('seq_menu_painel_id'),
-  titulo VARCHAR(255) NOT NULL,
-  ordem INT NOT NULL
-);
+INSERT INTO menu_painel(ID, TITULO, ORDEM) 
+  VALUES(DEFAULT, 'cadastros', 1);
 
-CREATE SEQUENCE seq_menu_item_id INCREMENT 1 START 1;
-CREATE TABLE menu_item (
-  id int4 PRIMARY KEY DEFAULT nextval('seq_menu_item_id'),
-  titulo VARCHAR(255) NOT NULL,
-  imagem VARCHAR(255) NOT NULL,
-  fxml_tela VARCHAR(255) NOT NULL,
-  fk_id_menu_painel int4 REFERENCES menu_painel(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ordem int NOT NULL
-);
-
-INSERT INTO menu_painel VALUES(DEFAULT, 'cadastros', 1);
-INSERT INTO menu_item VALUES(DEFAULT, 'usuario', 'user_16', 'UsuarioLista', 1, 1);
-INSERT INTO menu_item VALUES(DEFAULT, 'grupoUsuario', 'users_16', 'GrupoUsuarioLista', 1, 2);
+INSERT INTO menu_item(ID, TITULO, IMAGEM, FXML_TELA, ORDEM, FK_ID_MENU_PAINEL) 
+  VALUES(DEFAULT, 'usuario', 'user_16', 'UsuarioLista', 1, 1),
+        (DEFAULT, 'grupoUsuario', 'users_16', 'GrupoUsuarioLista', 1, 2);
